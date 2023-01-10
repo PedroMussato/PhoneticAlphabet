434d9fe3e5e0256b432af2ed56f145feb97a646f
pedromussato <pedrorenanmussato@gmail.com>
2023-01-10 11:41:31 -0300

    Initial commit

---
+++ PhoneticAlphabet.py
@@ -0,0 +1,39 @@
+phonetic_alphabet = {
+    'a': 'alpha',
+    'b': 'bravo',
+    'c': 'charlie',
+    'd': 'delta',
+    'e': 'echo',
+    'f': 'foxtrot',
+    'g': 'golf',
+    'h': 'hotel',
+    'i': 'india',
+    'j': 'juliet',
+    'k': 'kilo',
+    'l': 'lima',
+    'm': 'mike',
+    'n': 'november',
+    'o': 'oscar',
+    'p': 'papa',
+    'q': 'quebec',
+    'r': 'romeo',
+    's': 'sierra',
+    't': 'tango',
+    'u': 'uniform',
+    'v': 'victor',
+    'w': 'whiskey',
+    'x': 'x-ray',
+    'y': 'yankee',
+    'z': 'zulu',
+}
+
+while True:
+    frase = input('Type the frase: ')
+    frase = frase.lower()
+    print('\n')
+
+    for i in frase:
+        if i.isalpha():
+            print(phonetic_alphabet[i].capitalize())
+        else:
+            print(i)
