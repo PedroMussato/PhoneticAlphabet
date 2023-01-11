9ed3a5fba87acac91fa0490243c031922fe256d2
Pedro Mussato <tecnologia@pedromussato.com>
2023-01-10 23:21:57 -0300

    binary and bug correction

--- PhoneticAlphabet.py
+++ PhoneticAlphabet.py
@@ -5,7 +5,7 @@ phonetic_alphabet = {
     'm': 'mike', 'n': 'november', 'o': 'oscar', 'p': 'papa',
     'q': 'quebec', 'r': 'romeo', 's': 'sierra', 't': 'tango',
     'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'x-ray',
-    'y': 'yankee', 'z': 'zulu', '@': 'at', '.': 'dot', '0':'zero'
+    'y': 'yankee', 'z': 'zulu', '@': 'at', '.': 'dot', '0':'zero',
     '1':'one','2':'two','3':'three','4':'four','5':'five','6':'six',
     '7':'seven','8':'eight','9':'nine',
 }
